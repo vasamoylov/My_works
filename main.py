abf3548ca5244e989c641788b26861a1a5d0045c
vasamoylov <vfea-vitalik@mail.ru>
2024-06-11 14:31:32 +0300

    Commit

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-print('Hi, PyCharm')
-x = 43
-y = 32
-print(x * y)
-print("End line")
+import fake_math as fake_d
+import true_math as true_d
+result1 = fake_d.divide(69, 3)
+result2 = fake_d.divide(3, 0)
+result3 = true_d.divide(49, 7)
+result4 = true_d.divide(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
