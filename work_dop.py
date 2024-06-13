01503d2555ba91ed142eb3ec1b61d9a07eea98a8
vasamoylov <vfea-vitalik@mail.ru>
2024-06-13 14:06:49 +0300

    Commit

--- work_dop.py
+++ work_dop.py
@@ -1,13 +1,25 @@
 import itertools
-stuff = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-area = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+from collections import defaultdict
+from functools import reduce
+
 list_ = []
-#dict_ = {}
-for subset in itertools.combinations(stuff, 2):
-    for i in range(3, 6):
+list_2 = []
+for subset in itertools.combinations(range(1, 21), 2):
+    for i in range(3, 21):
         if i % sum(list(subset)) == 0:
-            #list_.append(list(subset))
-            dic=dict.fromkeys([i],list(subset))
-            for key, value in dic.items():
-                print(key, '-', *value)
-#print(list_)
+            list_.append(list(subset))
+            dic = dict.fromkeys([i], list(subset))
+            list_2.append(dic)
+
+def foo(r, d):
+    for k in d:
+        r[k].append(d[k])
+
+d = reduce(lambda r, d: foo(r, d) or r, list_2, defaultdict(list))
+for j in d:
+    if len(d[j]) > 1:
+        result = sum(d[j], [])
+        print(j, result)
+    else:
+        result = sum(d[j], [])
+        print(j, result)
