ec758777675fa177c2f534b2388154674d1f7a1a
vasamoylov <vfea-vitalik@mail.ru>
2024-07-04 21:03:36 +0300

    Commit

--- task 5_dop.py
+++ task 5_dop.py
@@ -6,11 +6,11 @@ class User:
         self.age = age
 
 class Video:
-    def __init__(self, title, duration):
+    def __init__(self, title, duration, adult_mode=False):
         self.title = title
         self.duration = duration
         self.time_now = 0
-        self.adult_mode = False
+        self.adult_mode = adult_mode
 
 class UrTube:
     def __init__(self):
