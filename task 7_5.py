d0d00eb705e2eb26801d8e2352cdcb526ba4beaf
vasamoylov <vfea-vitalik@mail.ru>
2024-07-23 08:41:47 +0300

    Commit

--- task 7_5.py
+++ task 7_5.py
@@ -1,10 +1,11 @@
 import os
 import time
-for root, dirs, files in os.walk(r'C:\Users\user\PycharmProjects\Lesson0'):
+directory = '.'
+for root, dirs, files in os.walk(directory):
   for file in files:
-    filepath = os.path.join(file)
-    filetime = os.path.getmtime(file)
+    filepath = os.path.join(root, file)
+    filetime = os.path.getmtime(filepath)
     formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
-    filesize = os.path.getsize(file)
-    parent_dir = os.path.dirname(file)
+    filesize = os.path.getsize(filepath)
+    parent_dir = os.path.dirname(filepath)
     print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time}, Родительская директория: {parent_dir}')
