0f4114d93d06a85740c1c64837f1f5f05ea0b91b
vasamoylov <vfea-vitalik@mail.ru>
2024-07-24 13:13:31 +0300

    Commit

---
+++ task 8_3.py
@@ -0,0 +1,70 @@
+class IncorrectVinNumber(Exception):
+    def __init__(self, message):
+        self.message = message
+class IncorrectCarNumbers(Exception):
+    def __init__(self, message):
+        self.message = message
+
+class Car:
+    def __init__(self, model, __vin, __numbers):
+        self.model = model
+        self.__vin = __vin
+        self.__numbers = __numbers
+        self.__is_valid_vin(__vin)
+        self.__is_valid_numbers(__numbers)
+    def set_models(self):
+        if self.model not in self.models:
+            self.models.append(self.model)
+
+
+    def __is_valid_vin(self, vin_number):
+        valid = True
+        if not isinstance(vin_number, int):
+            valid = False
+            raise IncorrectVinNumber('Некорректный тип vin номер')
+        if not 1000000 <= vin_number <= 9999999:
+            valid = False
+            raise IncorrectVinNumber('Неверный диапазон для vin номера')
+        return valid
+    def __is_valid_numbers(self, numbers):
+        valid = True
+        if not isinstance(numbers, str):
+            valid = False
+            raise IncorrectVinNumber('Некорректный тип данных для номеров')
+        if len(numbers) != 6:
+            valid = False
+            raise IncorrectVinNumber('Неверная длина номера')
+        return valid
+
+
+
+try:
+  first = Car('Model1', 1000000, 'f123dj')
+except IncorrectVinNumber as exc:
+  print(exc.message)
+except IncorrectCarNumbers as exc:
+  print(exc.message)
+else:
+  print(f'{first.model} успешно создан')
+
+try:
+  second = Car('Model2', 300, 'т001тр')
+except IncorrectVinNumber as exc:
+  print(exc.message)
+except IncorrectCarNumbers as exc:
+  print(exc.message)
+else:
+  print(f'{second.model} успешно создан')
+
+try:
+  third = Car('Model3', 2020202, 'нет номера')
+except IncorrectVinNumber as exc:
+  print(exc.message)
+except IncorrectCarNumbers as exc:
+  print(exc.message)
+else:
+  print(f'{third.model} успешно создан')
+
+
+
+
