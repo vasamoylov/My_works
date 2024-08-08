20fd366b8db3f49d3437d116c0d8bcfbc7314c85
vasamoylov <vfea-vitalik@mail.ru>
2024-08-08 22:24:14 +0300

    Commit

--- task 10_3.py
+++ task 10_3.py
@@ -2,16 +2,15 @@ from threading import Thread, Lock
 import random
 import time
 
-lock = Lock()
-
 
 class Bank:
     def __init__(self):
         self.balance = 0
+        self.lock = Lock()
 
     def deposit(self):
-        if lock.locked():
-            lock.release()
+        if self.lock.locked():
+            self.lock.release()
         for i in range(100):
             summ = random.randint(50, 500)
             self.balance += summ
@@ -22,16 +21,15 @@ class Bank:
         for i in range(100):
             summ = random.randint(50, 500)
             print(f'Запрос на {summ}')
-            if lock.locked():
-                lock.release()
             if self.balance >= summ:
                 self.balance -= summ
                 print(f'Снятие: {summ}. Баланс: {self.balance}')
                 time.sleep(0.001)
             else:
-                lock.acquire()
+                self.lock.acquire()
                 print('Запрос отклонён, недостаточно средств')
-                time.sleep(0.001)
+                #time.sleep(0.001)
+
 
 bk = Bank()
 
