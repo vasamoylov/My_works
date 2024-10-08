c5722d3e0f32a5c17f2cfa1e8c6dcc86f5f2d8d7
vasamoylov <vfea-vitalik@mail.ru>
2024-10-08 22:42:02 +0300

    Commit

---
+++ task_14_1.py
@@ -0,0 +1,30 @@
+import sqlite3
+
+connection = sqlite3.connect('not_telegram.db')
+cursor = connection.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users(
+id INTEGER PRIMARY KEY,
+username TEXT NOT NULL,
+email TEXT NOT NULL,
+age INTEGER,
+balance INTEGER NOT NULL)
+''')
+
+for i in range(1, 11):
+    cursor.execute('INSERT INTO Users (id, username, email, age, balance) VALUES (?, ?, ?, ?, ?)',
+                   (f'{i}', f'User{i}', f'example{i}@gmail.com', f'{i * 10}', '1000'))
+
+cursor.execute('UPDATE Users SET balance = ? WHERE ROWID % 2 != 0', ('500',))
+
+for i in range(1, 11, 3):
+    cursor.execute('DELETE FROM Users WHERE id = ?', (f'{i}',))
+
+cursor.execute('SELECT username, email, age, balance FROM Users WHERE age != ?', ('60',))
+users = cursor.fetchall()
+for user in users:
+    print(f'Имя: {user[0]} | Почта: {user[1]} | Возраст: {user[2]} | Баланс: {user[3]}')
+
+connection.commit()
+connection.close()
