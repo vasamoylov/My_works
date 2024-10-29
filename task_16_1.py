3c6afad2f795428cba6cd7465d6c00b250c66bf9
vasamoylov <vfea-vitalik@mail.ru>
2024-10-29 10:26:36 +0300

    Commit

---
+++ task_16_1.py
@@ -0,0 +1,24 @@
+import asyncio
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get('/')
+async def wellcome() -> str:
+    return f'Главная страница'
+
+
+@app.get('/user/admin')
+async def admin() -> str:
+    return f'Вы вошли как администратор'
+
+
+@app.get('/user/{user_id}')
+async def users(user_id: int) -> str:
+    return f'Вы вошли как пользователь № {user_id}'
+
+
+@app.get('/user')
+async def user_paginator(username: str, age: int) -> dict:
+    return {'User': username, 'Age': age}
